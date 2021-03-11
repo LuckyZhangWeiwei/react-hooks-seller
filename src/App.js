eaa84388fc43792648cf7546a9edf1f551f2b191
zhangweiwei <zhangzhang_10@hotmail.com>
2021-03-11 11:21:10 +0800

    init mock data create express server setup proxy to avoid cross cors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+
 function App() {
+  useEffect(() => {
+    axios.get('/api/seller').then(res => {
+      console.log(res)
+    })
+  }, [])
+
   return (
     <div>
       test
