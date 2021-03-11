6edb4490148cbccf22cf884aa5afc908791ba181
zhangweiwei <zhangzhang_10@hotmail.com>
2021-03-11 10:16:07 +0800

    init project

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -63,6 +63,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+const stylusRegex = /\.(styl|stylus)$/;
+const stylusModuleRegex = /\.module\.(styl|stylus)$/;
 
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
@@ -534,6 +536,17 @@ module.exports = function (webpackEnv) {
                 'sass-loader'
               ),
             },
+            {
+							test: stylusRegex,
+							exclude: stylusModuleRegex,
+							use: getStyleLoaders(
+                {
+                  importLoaders: 3,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                },
+                'stylus-loader'
+              )
+						},
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
