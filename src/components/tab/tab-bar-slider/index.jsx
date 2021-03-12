489ccf21b4519078ec6862e96018658ed01e9202
zhangweiwei <zhangzhang_10@hotmail.com>
2021-03-12 18:50:09 +0800

    change

---
+++ src/components/tab/tab-bar-slider/index.jsx
@@ -0,0 +1,9 @@
+import './index.styl'
+
+const TabBarSlider = props => {
+  return (
+    <div className="tab-bar-slider"></div>
+  )
+}
+
+export default TabBarSlider
--- src/components/tab/tab-bar/index.jsx
+++ src/components/tab/tab-bar/index.jsx
@@ -1,10 +1,13 @@
+import TabBarSlider from './../tab-bar-slider'
 import './index.styl'
+
 const TabBar = props => {
   return (
     <div className="tab">
       {
         props.children
       }
+      <TabBarSlider/>
     </div>
   )
 }
