a1bdc1daae0a8c4172b85c695e8f57474c2704f5
zhangweiwei <zhangzhang_10@hotmail.com>
2021-03-14 09:44:41 +0800

    change

--- src/components/tab/tab-bar-slider/index.jsx
+++ src/components/tab/tab-bar-slider/index.jsx
@@ -4,14 +4,14 @@ import './index.styl'
 const TabBarSlider = props => {
   const sliderRef = useRef(null)
   useEffect(() => {
-    sliderRef.current.style['transition'] = 'transform 0.2s linear'
+    // sliderRef.current.style['transition'] = 'transform 0.2s linear'
   }, [])
 
   useEffect(() => {
-    sliderRef.current.style['transform'] = `translateX(${props.transfromX * sliderRef.current.clientWidth}px) translateZ(0)`
+    sliderRef.current.style['transform'] = `translateX(${props.transfromX * 125}px) translateZ(0)`
   }, [props.transfromX])
   return (
-    <div ref={sliderRef} className="tab-bar-slider"></div>
+    <div ref={sliderRef} className="tab-bar-slider" />
   )
 }
 
