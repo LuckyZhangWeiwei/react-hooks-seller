0423ec0bceebecdc7c57346d494c4460d28105e4
zhangweiwei <zhangzhang_10@hotmail.com>
2021-03-18 15:25:07 +0800

    change

--- src/components/cart-control/index.jsx
+++ src/components/cart-control/index.jsx
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import './index.styl'
 
 const CartControl = props => {
+
   const { food } = props
+
+  const [showTransition, setShowTransition] = useState(false) 
+
+  useEffect(() => {
+    if (food.count) {
+      setShowTransition(true)
+    } else {
+      setShowTransition(false)
+    }
+  }, [food.count])
 
   const descrease = (e) => {
     e.stopPropagation()
-    props.onDescrease(food)
+    if (food.count === 1) {
+      setShowTransition(false)
+      setTimeout(() => {
+        props.onDescrease(food)  
+      }, 310);
+    } else {
+      props.onDescrease(food)  
+    }
   }
 
   const add = (e) => {
@@ -19,16 +39,27 @@ const CartControl = props => {
       food &&
       <div className="cart-control">
         {
-          food.count &&
-          <div className="cart-decrease" onClick={e => descrease(e)}>
-            <span className="inner icon-remove_circle_outline" />
-          </div>
+          food.count 
+          &&
+          <CSSTransition 
+            timeout={30000} 
+            classNames="move" 
+            in={showTransition}>
+            <div 
+              className="cart-decrease"
+              onClick={e => descrease(e)}>
+              <span className="inner icon-remove_circle_outline" />
+            </div>
+          </CSSTransition>
         }
         {
           food.count && 
           <div className="cart-count">{food.count}</div>
         }
-        <div className="cart-add icon-add_circle" onClick={e => add(e)} />
+        <div 
+          className="cart-add icon-add_circle" 
+          onClick={e => add(e)} 
+        />
       </div>
     }
     </>
